0e295ca4213ad90b39e685aab7d491ceaa897d25
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2025-05-03 15:02:00 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,4 @@
-from app import app ;
+from app import app
 
 if __name__ == "__main__":
-# Render sets the port dynamically, so bind to the PORT environment variable
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run()
